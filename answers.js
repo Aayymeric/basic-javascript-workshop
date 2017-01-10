753e3695cc11ddf08232f38a007e1f093f46f6d1
Aymeric Sauve <aymericsauve@gmail.com>
2017-01-10 22:36:30 +0000

    function add substract mult div

--- answers.js
+++ answers.js
@@ -1,16 +1,19 @@
-  //returns the first carachter of the string
+  //function that returns the first carachter of the string
+
 function firstLetter(str) { 
      return str[0]; 
 }
 console.log(firstLetter("Hello"))
  
-// return the last caracther of a string
+// function that returns the last caracther of a string
+
 function lastLetter(str) { 
     return str[str.length-1] 
 } 
 console.log(lastLetter("Hello"))  
 
-//  Write a function that takes a string and a number, and returns the character at the position represented by the number.
+//   function that takes a string and a number, and returns the character at the position represented by the number.
+ 
  function myFunction(str,result) {
      
      return str.charAt(result)
@@ -30,7 +33,31 @@ function twoNumbersSum(a,b)  {
  function multiply (a,b) { 
           return a * b 
 }
-console.log(multiply(4,12)); 
+console.log(multiply(4,12));  
+  
+ // function that returns the sum, difference, product & ratio 
+ 
+ function operator (a, b, str) {
+     
+     if (str === "add") {
+        return a+b  
+     }
+     else if  (str === "substract") {
+        return  a-b
+     } 
+     if (str === "mult") {
+     return  a * b
+     } 
+     if (str ==="div") { 
+         return a / b
+     } 
+     else { 
+         return 0  
+     }    
+     
+ };
+ console.log(operator(21,7, "div"))
+
 
 
      
