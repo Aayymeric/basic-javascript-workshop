a427b9bd78ffaf569abe46fc7c9b576d6531096a
Aymeric Sauve <aymericsauve@gmail.com>
2017-01-10 23:22:31 +0000

    hello hello function & for loop 1 to 10

--- answers.js
+++ answers.js
@@ -3,35 +3,35 @@
 function firstLetter(str) { 
      return str[0]; 
 }
-console.log(firstLetter("Hello"))
+console.log(firstLetter("Hello"));
  
 // function that returns the last caracther of a string
 
 function lastLetter(str) { 
-    return str[str.length-1] 
+    return str[str.length-1]; 
 } 
-console.log(lastLetter("Hello"))  
+console.log(lastLetter("Hello"));  
 
 //   function that takes a string and a number, and returns the character at the position represented by the number.
  
  function myFunction(str,result) {
      
-     return str.charAt(result)
+     return str.charAt(result);
  } 
-  console.log(myFunction("hello",4)) 
+  console.log(myFunction("hello",4)); 
   
   
  // function that adds two numbers 
  
 function twoNumbersSum(a,b)  {  
-    return a+b 
+    return a+b; 
 }  
     console.log(twoNumbersSum(1,15)); 
       
 // function that multiplies numbers  
  
  function multiply (a,b) { 
-          return a * b 
+          return a * b;
 }
 console.log(multiply(4,12));  
   
@@ -40,24 +40,42 @@ console.log(multiply(4,12));
  function operator (a, b, str) {
      
      if (str === "add") {
-        return a+b  
+        return a+b ; 
      }
      else if  (str === "substract") {
-        return  a-b
+        return  a-b;
      } 
      if (str === "mult") {
-     return  a * b
+     return  a * b;
      } 
      if (str ==="div") { 
-         return a / b
+         return a / b;
      } 
      else { 
-         return 0  
+         return 0;  
      }    
      
  };
- console.log(operator(21,7, "div"))
+ console.log(operator(21,7, "div"));
 
+// function that returns a string repeated that many number of times 
 
-
+function repeatString (str,a) { 
+    var b = ""
+    for (var i=0; i<a; i++) {  
+        b = b + str;
+    } 
+    return b;
+} 
+ console.log(repeatString("ham",3)); 
+ 
+ //
+ 
+ function forLoop () { 
+       for (var i=0; i<=10; i++) {
+           console.log(i)
+       } 
+       
+};
+   forLoop();     
      
